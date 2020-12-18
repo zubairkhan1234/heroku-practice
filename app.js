296662a36c39853d6523bea35659b7b5fec4cdc1
Muhammad Zubair <zubairsaleem1210@gmail.com>
2020-12-18 16:34:48 +0500

    some work

---
+++ app.js
@@ -0,0 +1,22 @@
+var express = require("express")
+var server = express();
+const PORT = process.env.PORT || 5000
+
+server.get ("/", ( req, res, next) =>{
+    console.log("some one get menue");
+    res.get ("what do you want? fodd or water")
+})
+
+server.get ("/water", ( req, res, next) =>{
+    console.log("some one is asking water");
+    res.get ("This is water")
+})
+
+server.get ("/food", ( req, res, next) =>{
+    console.log("some one is asking food");
+    res.get ("This is food")
+})
+ 
+server.listen(PORT, () => {
+    console.log("server is running on port " + PORT);
+})
