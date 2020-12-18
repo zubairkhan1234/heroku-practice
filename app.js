a692a19dde70694cb378bcc0bf169433746ff2e9
Muhammad Zubair <zubairsaleem1210@gmail.com>
2020-12-18 17:34:12 +0500

    ook

--- app.js
+++ app.js
@@ -1,23 +1,23 @@
 
 var PORT = process.env.PORT || 5000;
-var express = require("express")
+var express = require("express");
 
 
 var server = express();
 
 server.get("/", (req, res, next) => {
     console.log("some one get menue");
-    res.get("what do you want? fodd or water")
+    res.send("what do you want? fodd or water")
 })
 
 server.get("/water", (req, res, next) => {
     console.log("some one is asking water");
-    res.get("This is water")
+    res.send("This is water")
 })
 
 server.get("/food", (req, res, next) => {
     console.log("some one is asking food");
-    res.get("This is food")
+    res.send("This is food")
 })
 
 server.listen(PORT, () => {
